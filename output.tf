54ac796e54819cd89183c311c6c5371c62847b7f
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-07-01 08:52:26 -0400

    add depends_on to output

--- output.tf
+++ output.tf
@@ -1,5 +1,9 @@
-
 output "name" {
-  value = "${lower(var.domain_prefix)}.lnrisk.io"
+  value       = "${lower(var.domain_prefix)}.lnrisk.io"
   description = "The DNS zone that has been delegated to you"
+  depends_on  = [
+    # ensure resources are created prior to outputs
+    azurerm_dns_ns_record.iog,
+    azurerm_dns_zone.sre
+  ]
 }
