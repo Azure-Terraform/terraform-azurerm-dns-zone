42c278968b7abee5f513ba10c6e3712e12e6e87b
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-10-22 15:03:57 -0400

    add outputs for resource_group and subscription_id

--- output.tf
+++ output.tf
@@ -4,11 +4,21 @@ output "id" {
 }
 
 output "name" {
-  value       = "${lower(var.child_domain_prefix)}.${lower(var.parent_domain)}"
   description = "The DNS zone that has been delegated to you"
+  value       = "${lower(var.child_domain_prefix)}.${lower(var.parent_domain)}"
   depends_on  = [
     # ensure resources are created prior to outputs
     azurerm_dns_ns_record.child,
     azurerm_dns_zone.child
   ]
+}
+
+output "resource_group_name" {
+  description = "The resource group which contains the child zone"
+  value       = var.child_domain_resource_group_name
+}
+
+output "subscription_id" {
+  description = "The subscription_id which contains the child zone"
+  value       = var.child_domain_subscription_id
 }
